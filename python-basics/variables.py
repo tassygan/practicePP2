21a7416c2a1175273fd67ef23719d3888f5b1aa4
Aidana Tassygan <aidanatn@MacBook-Air-Aidana.local>
2026-01-24 21:32:16 +0500

    Variable types and namaes added

---
+++ python-basics/variables.py
@@ -0,0 +1,35 @@
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#Variables do not need to be declared
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#how to get the data type 
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#multiple variables in one line
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#unpacking
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
