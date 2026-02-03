c6fb00707b02a8c1fc7d95050a278cf6aafd0fb8
Aidana Tassygan <aidanatn@MacBook-Air-Aidana.local>
2026-02-03 14:28:26 +0500

    if statement

---
+++ practice 2/if_else/if_statement.py
@@ -0,0 +1,16 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#Multiple statements in an if block:
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+#Using a boolean variable:
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
