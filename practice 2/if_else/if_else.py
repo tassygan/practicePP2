c0033932b8346b318e63194558a57384671c0efc
Aidana Tassygan <aidanatn@MacBook-Air-Aidana.local>
2026-02-03 14:30:18 +0500

    if else statements

---
+++ practice 2/if_else/if_else.py
@@ -0,0 +1,25 @@
+#ex 1
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+#ex 2
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+#ex 3
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
