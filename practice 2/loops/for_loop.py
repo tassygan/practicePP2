c9c8f430d55389aaf4955f68cdb6624857849f06
Aidana Tassygan <aidanatn@MacBook-Air-Aidana.local>
2026-02-03 14:41:38 +0500

    for loop files added

---
+++ practice 2/loops/for_loop.py
@@ -0,0 +1,11 @@
+'''ExampleGet your own Python Server
+Print each fruit in a fruit list:'''
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+#Loop through the letters in the word "banana":
+for x in "banana":
+  print(x)
