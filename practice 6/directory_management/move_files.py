49140d590434efc1147669f8950f042c9a5eb183
Aidana Tassygan <aidanatn@MacBook-Air-Aidana-3.local>
2026-03-17 16:46:36 +0500

    fix

--- practice 6/directory_management/move_files.py
+++ practice 6/directory_management/move_files.py
@@ -5,7 +5,7 @@ os.makedirs("folderA", exist_ok=True)
 os.makedirs("folderB", exist_ok=True)
 
 with open("folderA/test.txt", "w", encoding="utf-8") as f:
-    f.write("Hello from folderA")
+    f.write("folderA")
 
 shutil.copy("folderA/test.txt", "folderB/test_copy.txt")
 print("file copied to folderB.")
